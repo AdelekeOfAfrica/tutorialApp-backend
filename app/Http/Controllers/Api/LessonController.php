17bdaabe284aee1f78e143eaa71de5202e4685af
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2024-01-07 17:05:17 +0100

    added the api for lesson details

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -31,4 +31,25 @@ class LessonController extends Controller
         }
 
     }
+
+    public function lessonDetail(Request $request){
+        $id = $request->id;
+
+        try{
+           $result = Lesson::where('id', '=', $id)->select('name','description','thumbnail','video')->get();
+    
+           return response()->json([
+            'code'=>200,
+            'message'=>'Lesson details is here ',
+            'data'=>$result
+           ],200);
+        }catch(\Throwable $e){
+            return response()->json([
+                'code'=>500,
+                'message'=>'There was an error fetching the Lesson Details',
+                'data'=>$e->getMessage(),
+               ],500);
+        }
+
+    }
 }
